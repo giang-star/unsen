f97bd7c4e8afa5a1e6963975c19d7d50f17cd70f
giang-star <nvgianga9@gmail.com>
2022-09-23 10:44:36 +0700

    updates1

--- asess/app.js
+++ asess/app.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
         infinite:true,
         dots:false,
         
-        // autoplay:true,
-        // autoplaySpeed:1000,
+        autoplay:true,
+        autoplaySpeed:1000,
         prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
         nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
         responsive: [
